460e663a178edeaec5297a3432a98fa24e9c3dfc
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-10-15 22:44:14 -0700

    finished part 9, next is 10:

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f11792fc99be1ffa70610b014d0fefb021e48a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39186733ad6589632433c3d99f31dec847a9430c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
